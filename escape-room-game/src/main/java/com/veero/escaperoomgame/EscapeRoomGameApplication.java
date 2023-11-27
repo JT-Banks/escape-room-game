a9738829a552128719096aa93b29b670a3aab748
JT-Banks <jbank54@lsu.edu>
2023-11-27 09:45:11 -0600

    initial commit

---
+++ escape-room-game/src/main/java/com/veero/escaperoomgame/EscapeRoomGameApplication.java
@@ -0,0 +1,13 @@
+package com.veero.escaperoomgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeRoomGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapeRoomGameApplication.class, args);
+	}
+
+}
