1a07fc4252b2eed695c38f096bca16c087273d1b
JT-Banks <jbank54@lsu.edu>
2023-12-07 21:57:42 -0600

    restructuring game objects within database

---
+++ src/main/java/com/veero/escaperoomgame/asylum/model/Action.java
@@ -0,0 +1,9 @@
+package com.veero.escaperoomgame.asylum.model;
+
+import lombok.Data;
+
+@Data
+public class Action {
+    private String actionType;
+    private String result;
+}
--- src/main/java/com/veero/escaperoomgame/asylum/model/GameObject.java
+++ src/main/java/com/veero/escaperoomgame/asylum/model/GameObject.java
@@ -4,13 +4,19 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @Document(collection = "game_objects")
 public class GameObject {
     @Id
     private String id;
-    private String interactionId;
-    private String roomId;
     private String name;
-    private InteractionType interactionType;
+    private String interactionType;
+    private String roomId;
+    private String interactionId;
+    private String description;
+    private List<String> clues;
+    private List<Action> actions;
+    private List<String> relatedObjects;
 }
