1d2ed36d57838356b64f0c7fede9c3a1ed618fd5
JT-Banks <JT@Ds-MacBook-Pro.local>
2025-12-11 18:52:44 -0600

    possibly wip

--- src/main/java/com/veero/escaperoomgame/asylum/controllers/RoomController.java
+++ src/main/java/com/veero/escaperoomgame/asylum/controllers/RoomController.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class RoomController {
 
    @Autowired
-   private RoomService roomService;
+   private final RoomService roomService;
 
     @Autowired
     public RoomController(RoomService roomService) {
